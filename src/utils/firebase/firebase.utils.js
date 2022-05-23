316a7cca5e5dbcd6c63106a6f7ee746893c58814
Enma <Bahran2005@gmail.com>
2022-05-23 10:52:18 +0300

    same shit

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,8 @@ import {
     getAuth,
     signInWithRedirect,
     signInWithPopup,
-    GoogleAuthProvider 
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth';
 import {    
     getFirestore,
@@ -26,18 +27,21 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
 prompt: 'select_account'
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+    if(!userAuth) return;
+
     const userDocRef = doc(db, 'users', userAuth.uid);
 
     const userSnapShot = await getDoc(userDocRef);
@@ -50,20 +54,19 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation,
             });
         } catch (erroe) {
             console.log('error creating the user', erroe.message);
         }
-
-        return userDocRef;
     }
 
-    // if user data does not exists
+    return userDocRef;
+};
 
-    // create / set the document with the data from userAuth in my collection
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
 
-    // if user data exists
-
-    // return userDocRef
+    return await createUserWithEmailAndPassword(auth, email, password)
 }
